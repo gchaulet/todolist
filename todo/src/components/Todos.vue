382354d951462fa1a57ae863ea0b7e355e5fefa5
guillaume <guillaumechaulet@hotmail.fr>
2020-05-11 11:39:08 +0200

    todo vue first

---
+++ todo/src/components/Todos.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+        </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in todos" v-bind:key="todo">
+                    <div class="view">
+                        <label for="">{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            todos: [{
+                name: 'Tache de test',
+                completed: false
+            }],
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todos.push ({
+                completed: false,
+                name: this.newTodo
+            })
+            this.newTodo = ''
+        }
+    }
+  
+}
+</script>
+
+<style src="./todos.css">
+
+</style>
