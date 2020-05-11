f0064a7c8b3f8efeccc18ee39856c186a27c05d1
guillaume <guillaumechaulet@hotmail.fr>
2020-05-11 18:11:01 +0200

    todo vue third 3 filters  task

--- todo/src/components/Todos.vue
+++ todo/src/components/Todos.vue
@@ -6,7 +6,7 @@
         </header>
         <div class="main">
             <ul class="todo-list">
-                <li class="todo " v-for="todo in todos" v-bind:key="todo" :class="{completed : todo.completed}">
+                <li class="todo " v-for="todo in filteredTodos" v-bind:key="todo" :class="{completed : todo.completed}">
                     <div class="view">
                         <input type="checkbox" v-model="todo.completed" class="toggle">
                         <label for="">{{ todo.name }}</label>
@@ -14,7 +14,13 @@
                 </li>
             </ul>
         </div>
-        <footer class="footer"><strong>{{ remaining }}</strong> tâches à faire</footer>
+        <footer class="footer"><strong>{{ remaining }}</strong> tâches à faire
+        <ul class="filters">
+            <li><a href="" :class="{selected: filter === 'all'}" @click.prevent="filter ='all'">Toutes</a></li>
+            <li><a href="" :class="{selected: filter === 'todo'}" @click.prevent="filter ='todo'">A faire</a></li>
+            <li><a href="" :class="{selected: filter === 'done'}" @click.prevent="filter ='done'">Faites</a></li>
+        </ul>
+        </footer>
     </section>
 </template>
 
@@ -27,7 +33,8 @@ export default {
                 name: 'Tache de test',
                 completed: false
             }],
-            newTodo: ''
+            newTodo: '',
+            filter: 'all'
         }
     },
     methods: {
@@ -42,6 +49,14 @@ export default {
     computed: {
         remaining() {
             return this.todos.filter(todo => !todo.completed).length
+        },
+        filteredTodos (){
+            if(this.filter === 'todo'){
+                return this.todos.filter(todo => !todo.completed)
+            } else if (this.filter === 'done'){
+                return this.todos.filter(todo => todo.completed)
+            }
+            return this.todos
         }
     }
   
